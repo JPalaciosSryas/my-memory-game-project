cf45c8f71d4a3a183fd95aec0a5167ee2e1ee571
JPalaciosSryas <juan.palacios@sryas.com>
2022-11-09 08:18:58 -0600

    randomizing cards

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
+
 let image1 = "url('./assets/goku-pelon.jfif')"
 let image2 = "url('./assets/auron.jfif')"
 let image3 = "url('./assets/explosion.jpg')"
 let image4 = "url('./assets/dora.jfif')"
 let image5 = "url('./assets/mrclean.jpg')"
 let image6 = "url('./assets/richi-phelps.jfif')"
-let image7 = "url('./assets/goku-pelon.jfif')"
-let image8 = "url('./assets/auron.jfif')"
-let image9 = "url('./assets/explosion.jpg')"
-let image10 = "url('./assets/dora.jfif')"
-let image11 = "url('./assets/mrclean.jpg')"
-let image12 = "url('./assets/richi-phelps.jfif')"
 
+let oddCards = document.querySelectorAll(".odd");
+let evenCards = document.querySelectorAll(".even");
 
-let cards = document.querySelectorAll(".inner-card");
+const images = [image1, image2, image3, image4, image5, image6];
 
-const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+const randomizeImages = () => {
+    //Generate an array of random numbers between 0 and 5
 
-const randomImages = (arr) => {
-    let currentIndex = Math.floor(Math.random()*images.length);
-    let currentEle = arr[currentIndex];
-    console.log(currentEle);
-    return currentEle;
+    const randomNumbers = (n) => {
+        let arr = [];
+        while (arr.length < n) {
+            let num = Math.floor(Math.random() * n);
+            if (arr.includes(num)) {
+                continue
+            } else {
+                arr.push(num);
+            }
+        }
+        return arr;
+    }
+
+    //use randomNumbers() to generate two arrays
+
+    let order1 = randomNumbers(6);
+    let order2 = randomNumbers(6);
+
+    //use the arrays' values as indexes of the image's array
+
+    oddCards.forEach(card => {
+        let removed1 = order1.splice(0, 1).pop();
+        card.style.backgroundImage = images[removed1];
+    })
+
+    evenCards.forEach(card => {
+        let removed2 = order2.splice(0, 1).pop();
+        card.style.backgroundImage = images[removed2];
+    })
 }
 
+randomizeImages();
 
+let inactiveCards = document.querySelectorAll("inactive");
 
-cards.forEach(card => {
-    card.style.backgroundImage = randomImages(images);
-})
+let activeCards = document.querySelectorAll("active");
